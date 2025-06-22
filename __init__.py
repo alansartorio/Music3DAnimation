328f220312e439d79ab59e6ee0ce9d911e1752e8
Alan Sartorio <alan42ga@hotmail.com>
2025-06-22 03:15:42 -0300

    use animated custom properties to store director data

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
+from collections import namedtuple
 import bpy
 from pathlib import Path
 
-from typing import TYPE_CHECKING, assert_never
+from typing import TYPE_CHECKING, Iterable, NamedTuple, Optional, assert_never
 
-from bpy.types import Panel, PropertyGroup
+from bpy.types import (
+    Panel,
+    PropertyGroup,
+)
 from bpy.props import PointerProperty, StringProperty
 import json
 
@@ -30,8 +34,6 @@ class AnimusicGeneratorPanel(Panel):
     bl_category = "Tool"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    # bl_context = "object"
-    # bl_options = {'DEFAULT_CLOSED'}
 
     @classmethod
     def poll(cls, context):
@@ -42,14 +44,6 @@ class AnimusicGeneratorPanel(Panel):
         assert layout is not None
         layout.label(text="Animusic")
 
-    def run(self, context):
-        scene = context.scene
-        assert scene is not None
-
-        obj = scene.objects["Target"]
-        obj.location = demo1.note_to_position(0, 10)
-        obj.keyframe_insert(data_path="location", frame=1)
-
     def draw(self, context):
         layout = self.layout
         assert layout is not None
@@ -59,6 +53,13 @@ class AnimusicGeneratorPanel(Panel):
         col = layout.column(align=True)
         col.prop(scene.animusic, "director_data", text="")
         col.operator("object.generate_animusic_keyframes", text="Import")
+
+
+class ActuatorKeyFrame(NamedTuple):
+    frame: int
+    action: bool
+    track: int
+    note: int
 
 
 class AnimusicGenerate(bpy.types.Operator):
@@ -74,7 +75,7 @@ class AnimusicGenerate(bpy.types.Operator):
         print(props.director_data)
         director_data_path = Path(
             bpy.path.abspath(props.director_data)
-        )  # make a path object of abs path
+        )
 
         assert director_data_path.exists()
 
@@ -82,6 +83,48 @@ class AnimusicGenerate(bpy.types.Operator):
             director_data = DirectorData.from_json(json.load(director_data_file))
 
         print(director_data)
+
+        actuators_collection = bpy.data.collections.get("actuators")
+        if actuators_collection is None:
+            actuators_collection = bpy.data.collections.new(name="actuators")
+            scene.collection.children.link(actuators_collection)
+        assert actuators_collection is not None
+
+        fps = scene.render.fps
+        for actuator in director_data.actuators:
+            obj = actuators_collection.objects.get(actuator.name)
+            if obj is None:
+                obj = bpy.data.objects.new(name=actuator.name, object_data=None)
+                actuators_collection.objects.link(obj)
+            assert obj is not None
+
+            obj.animation_data_clear()
+
+            frame = 0
+            previous_hit = frame
+
+            def add_keyframe(kf: ActuatorKeyFrame):
+                obj["action"] = kf.action
+                obj["track"] = kf.track
+                obj["note"] = kf.note
+                obj.keyframe_insert('["action"]', frame=kf.frame)
+                obj.keyframe_insert('["track"]', frame=kf.frame)
+                obj.keyframe_insert('["note"]', frame=kf.frame)
+
+            for note in actuator.notes:
+                frame += note.delta * fps
+                end_preparation = max(frame - demo1.prepare_time * fps, previous_hit)
+                complete_animation = frame + demo1.release_time * fps
+                add_keyframe(
+                    ActuatorKeyFrame(int(end_preparation), False, note.track, note.note)
+                )
+                add_keyframe(ActuatorKeyFrame(int(frame), True, note.track, note.note))
+                add_keyframe(
+                    ActuatorKeyFrame(
+                        int(complete_animation), False, note.track, note.note
+                    )
+                )
+                previous_hit = complete_animation
 
         return {"FINISHED"}
 
--- demo1.py
+++ demo1.py
@@ -1,4 +1,7 @@
 import mathutils
 
-def note_to_position(track: int, note: int) -> mathutils.Vector:
-    return mathutils.Vector((note, 0, 0))
+def note_to_position(track: int, note: int, preparing: bool) -> mathutils.Vector:
+    return mathutils.Vector((note, 0, 5 if preparing else 0))
+
+prepare_time = 0.1
+release_time = 0.2
